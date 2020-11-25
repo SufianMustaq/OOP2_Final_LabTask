380e00cd8982324fa62e55b28de77e3cc2b374fd
SufianMustaq <sufianmustaq3599@gmail.com>
2020-11-26 01:16:45 +0600

    Test Interface 1 done

---
+++ Test Interface 1/BasicCalculetor/ScientificCalculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculetor
+{
+    interface ScientificCalculator
+    {
+        void sc(int x, int y);
+    }
+}
---
+++ Test Interface 1/BasicCalculetor/basicCalculators.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicCalculetor
+{
+    interface basicCalculators
+    {
+        void Sum(int x, int y);
+        void Sub(int x, int y);
+        void Multiplication(int x, int y);
+        void Devision(int x, int y);
+
+
+    }
+}
