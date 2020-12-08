57419a6791f981b19432cee9da5194baba99e9db
SufianMustaq <sufianmustaq3599@gmail.com>
2020-12-09 01:10:17 +0600

    Form_practice

---
+++ FORM_Practice/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FORM_Practice
+{
+    static class Program
+    {
+       
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form2());
+        }
+    }
+}
